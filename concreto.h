29fd6412f7d7113524b7a7012a5c471c823774aa
Thomas Hansen <thomasphansen@users.noreply.github.com>
2025-06-13 14:55:56 +0200

    Refatore programa e adicione testes

---
+++ concreto.h
@@ -0,0 +1,33 @@
+#ifndef CONCRETO_H
+#define CONCRETO_H
+
+#define DENSITY_CEMENT 1440.0
+#define DENSITY_SAND 1600.0
+#define DENSITY_GRAVEL 1550.0
+#define DENSITY_WATER 1000.0
+
+typedef struct {
+    double cement;
+    double sand;
+    double gravel;
+} MixRatio;
+
+typedef struct {
+    double cement;
+    double sand;
+    double gravel;
+} Volumes;
+
+typedef struct {
+    double cement;
+    double sand;
+    double gravel;
+    double water;
+} Masses;
+
+int parse_ratio(const char *str, MixRatio *ratio);
+double wc_ratio(double granulometry);
+Volumes compute_volumes(double total_volume, MixRatio ratio);
+Masses compute_masses(Volumes volumes, double granulometry);
+
+#endif // CONCRETO_H
